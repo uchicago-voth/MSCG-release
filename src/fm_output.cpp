d19d9e22bfddad26fabc381f65910c4bb97f8657
Jacob Wagner <jwwagner@Jacobs-MacBook.local>
2017-05-18 19:07:48 -0500

    get_basename

--- src/fm_output.cpp
+++ src/fm_output.cpp
@@ -387,11 +387,7 @@ void write_one_param_table_files(InteractionClassComputer* const icomp, char ** 
 void write_two_param_bspline_table_file(InteractionClassComputer* const icomp, char ** const name, MATRIX_DATA* const mat, const int index_among_defined)
 {
     // Print out a table of the interaction forces.
-    std::string filename = icomp->ispec->get_interaction_name(name, index_among_defined, "_");
-    if (!icomp->ispec->get_short_name().empty()) {
-        filename += "_" + icomp->ispec->get_short_name();
-    }
- 	filename += ".dat";
+    std::string filename = ispec->get_basename(name, index_among_defined_intrxns, "_") + ".dat";
     FILE* curr_spline_output_file = open_file(filename.c_str(), "w");
 	std::vector<double> axis_vals, force_vals, deriv_vals;
 	icomp->calc_grid_of_force_and_deriv_vals(mat->fm_solution, index_among_defined, icomp->ispec->output_binwidth, axis_vals, force_vals, deriv_vals);
@@ -571,10 +567,7 @@ void write_bootstrapping_one_param_table_files(InteractionClassComputer* const i
 	}
     
     // Print out tabulated output files in MSCGFM style and LAMMPS style.
-    std::string basename = icomp->ispec->get_interaction_name(name, index_among_defined_intrxns, "_");
-    if (!icomp->ispec->get_short_name().empty()) {
-        basename += "_" + icomp->ispec->get_short_name();
-    }
+    basename = ispec->get_basename(name, index_among_defined_intrxns, "_");
 
     // Select symmetric basename modifier if appropriate (i.e. DOOM interactions)
 	if(icomp->ispec->defined_to_symmetric_intrxn_index_map[index_among_defined_intrxns] != 0) {
