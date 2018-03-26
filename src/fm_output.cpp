8153fe0f1c066b742098546e92be1461bedb0aeb
Alexander Jin Pak <ajpak@midway-login1.rcc.local>
2018-03-26 13:18:11 -0500

    Changed a misnamed index_among_defined to index_among_defined_intrxns

--- src/fm_output.cpp
+++ src/fm_output.cpp
@@ -677,7 +677,7 @@ void write_bootstrapping_one_param_bspline_file(InteractionClassComputer* const 
     for (int i = 0; i < mat->bootstrapping_num_estimates; i++) {
     	// Print the spline coefficients.
     	for (unsigned k = 0; k < interaction_column_indices; k++) {
-	  fprintf(spline_output_filep, "%.15le ", mat->fm_solution[icomp->interaction_class_column_index + icomp->ispec->interaction_column_indices[icomp->ispec->defined_to_matched_intrxn_index_map[index_among_defined] - 1] + k]);
+	  fprintf(spline_output_filep, "%.15le ", mat->fm_solution[icomp->interaction_class_column_index + icomp->ispec->interaction_column_indices[icomp->ispec->defined_to_matched_intrxn_index_map[index_among_defined_intrxns] - 1] + k]);
     	}
     
 	    // Complete the line and flush.
